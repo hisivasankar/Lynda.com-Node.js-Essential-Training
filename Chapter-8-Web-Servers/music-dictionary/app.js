6853bfa0ad4cab73dc7fdd7f646d048151c2acdb
Sivasankar D <ctsiva25@gmail.com>
2017-08-27 21:40:49 +0530

    Completed music-dictionary project
    Summary:
    
        music-dictionary is a small website which is developed in node.js
    using the following module.
       * express
       * body-parser
       * cors
    
    The project supports
       * Listing of known music terms
       * Adding new word to dictionary
       * Deleting a word

--- Chapter-8-Web-Servers/music-dictionary/app.js
+++ Chapter-8-Web-Servers/music-dictionary/app.js
@@ -9,23 +9,37 @@ var SERVER_PORT = 3000;
 var server = express();
 
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-server.use(function(req, res, next){
-	util.log(`${req.method} -> ${req.url}`);
-	next();
+server.use(function(req, res, next) {
+    util.log(`${req.method} -> ${req.url}`);
+    next();
 });
 
 server.use(cors());
 
 server.get('/dictionary-api', function(req, res) {
-	res.json(aDictionaryData);
+    res.json(aDictionaryData);
 });
 
-server.post('/dictionary-api', function(req, res){
-	res.json({"status": "cool"});
-	console.log(res.body);
-	//aDictionaryData.push(res.body);
+server.post('/dictionary-api', function(req, res) {
+    var word = {
+        'term': req.body.term,
+        'definition': req.body.definition
+    };
+    aDictionaryData.push(word);
+    res.json(word);
+    console.log(`New Word: %J`, word);
+});
+
+server.delete("/dictionary-api/:term", function(req, res) {
+    var sWordToBeDeleted = req.params.term;
+    aDictionaryData = aDictionaryData.filter(function(word) {
+        return word.term !== sWordToBeDeleted;
+    });
+    res.json(aDictionaryData);
 });
 
 server.use(express.static('./public'));
--- Chapter-8-Web-Servers/music-dictionary/public/dictionary-handler.js
+++ Chapter-8-Web-Servers/music-dictionary/public/dictionary-handler.js
@@ -1,45 +1,56 @@
 (function() {
     "use strict";
 
+    function addWordToDom(word) {
+        var dictionaryContainer = document.querySelector("#dict-list");
+        var wordHeader = document.createElement("h2"),
+            term = document.createTextNode(word.term),
+            definition = document.createTextNode(word.definition),
+            dt = document.createElement("dt"),
+            dd = document.createElement("dd");
+
+        wordHeader.appendChild(term);
+        dt.appendChild(wordHeader);
+        dd.appendChild(definition);
+
+        dictionaryContainer.appendChild(dt);
+        dictionaryContainer.appendChild(dd);
+    }
+
     function loadWords(aWords) {
         aWords = aWords || [];
-        var dictionaryContainer = document.querySelector("#dict-list");
         aWords.forEach(function(word) {
-            var wordHeader = document.createElement("h2"),
-                term = document.createTextNode(word.term),
-                definition = document.createTextNode(word.definition),
-                dt = document.createElement("dt"),
-                dd = document.createElement("dd");
-
-            wordHeader.appendChild(term);
-            dt.appendChild(wordHeader);
-            dd.appendChild(definition);
-
-            dictionaryContainer.appendChild(dt);
-            dictionaryContainer.appendChild(dd);
+            addWordToDom(word);
         });
     }
 
+
+
     function addWord(sWord) {
-    	console.log("Adding word..!");
-    	/*var oData = {
-    		"term": document.querySelector("#term").textContent,
-    		"definition": document.querySelector("#definition").textContent,
-    	};*/
+        console.log("Adding word..!");
+        var oData = {
+            "term": document.querySelector("#term").value,
+            "definition": document.querySelector("#definition").value,
+        };
 
-    	var oData = {
-    		"term": "stupid"
-    	};
+        var payload = JSON.stringify(oData);
 
-    	var payload = JSON.stringify(oData);
-
-    	makeRequest("/dictionary-api", "POST", payload, function(xhr, statusCode, data){
-    		console.log("Done!");
-    	});
+        makeRequest("/dictionary-api", "POST", payload, function(xhr, statusCode, data) {
+            var word = JSON.parse(data);
+            addWordToDom(word);
+        });
     }
 
     function deleteWord(sWord) {
-
+        var sUrl = "/dictionary-api/" + sWord;
+        makeRequest(sUrl, "DELETE", null, function(xhr, statusCode, data) {
+            if (statusCode === 200) {
+                var list = document.querySelector("#dict-list");
+                list.innerHTML = "";
+                var aWords = JSON.parse(data);
+                loadWords(aWords);
+            }
+        }, errorHandler);
     }
 
     function makeRequest(sUrl, sMethod, oData, fnSuccessCallback, fnFailureCallback) {
@@ -51,13 +62,12 @@
         xhr.addEventListener('error', fnFailureCallback);
         xhr.open(sMethod, sUrl);
         console.log(`${sMethod} -> ${sUrl}`);
-        if(oData) {
-        	xhr.setRequestHeader("Content-Type", "text/plain");
-        	xhr.send(oData);
+        if (oData) {
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(oData);
         } else {
-        	xhr.send(null);
+            xhr.send(null);
         }
-        
     }
 
     function errorHandler(err) {
@@ -77,6 +87,17 @@
         }, errorHandler);
     }
 
+
+    function addClickEvent() {
+        var dt = document.querySelector("#dict-list");
+        dt.addEventListener('click', function(event) {
+            if (event.target && event.target.parentElement.nodeName === "DT") {
+                var sTerm = event.target.innerText;
+                deleteWord(sTerm);
+            }
+        });
+    }
+
     function attachHandlers() {
         if (document.addEventListener) {
             //document.addEventListener("DOMContentLoaded", loadDictionary, false);
@@ -86,6 +107,7 @@
         }
 
         document.querySelector("#addWord").addEventListener("click", addWord);
+        addClickEvent();
     };
 
     attachHandlers();
