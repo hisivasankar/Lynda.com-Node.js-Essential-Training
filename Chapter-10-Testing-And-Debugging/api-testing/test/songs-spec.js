4d879487919de61d799807d0cb1b607438f586de
Sivasankar D <ctsiva25@gmail.com>
2017-09-17 23:16:01 +0530

    Added tests with spies
    Summary:
    
       Spies can be used to check if a particular function has been called or not.
    
    Module:
       - sinon

--- Chapter-10-Testing-And-Debugging/api-testing/test/songs-spec.js
+++ Chapter-10-Testing-And-Debugging/api-testing/test/songs-spec.js
@@ -1,5 +1,6 @@
 var expect = require("chai").expect,
-    rewire = require("rewire");
+    rewire = require("rewire"),
+    sinon = require("sinon");
 
 var songs = rewire("../songs.js");
 
@@ -7,17 +8,23 @@ describe("Songs Spec", function() {
     describe("addToPlaylist()", function() {
         beforeEach(function() {
             this._songs = ["AAA", "BBB"];
+            this.console = {
+            	log: sinon.spy()
+            };
             songs.__set__("songs", this._songs);
+            songs.__set__("console", this.console);
         });
 
         it("should add 'AAA' to playlist", function() {
             var bResult = songs.addToPlaylist('AAA');
             expect(bResult).to.be.true;
+            expect(this.console.log.callCount).to.equal(1);
         });
 
         it("shouldn't add 'CCC' to playlist", function() {
             var bResult = songs.addToPlaylist('CCC');
             expect(bResult).to.be.false;
+            expect(this.console.log.callCount).to.equal(1);
         });
     });
 });
