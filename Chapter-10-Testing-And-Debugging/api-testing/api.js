b7dd6f24ab05bf55624665dae354bc9fdec76736
Sivasankar D <ctsiva25@gmail.com>
2017-09-17 22:25:50 +0530

    Added a Async Test cases
    Summary:
      Testing a async function with the help of nock npm module

--- Chapter-10-Testing-And-Debugging/api-testing/api.js
+++ Chapter-10-Testing-And-Debugging/api-testing/api.js
@@ -1,5 +1,21 @@
+var https = require("https");
+
 module.exports = {
-	sayHello(sName) {
-		return `Hello, ${sName}`;
-	}
+    sayHello(sName) {
+        return `Hello, ${sName}`;
+    },
+
+    loadWiki(person, callback) {
+        var sUrl = `https://en.wikipedia.org/wiki/${person.first}_${person.last}`;
+        https.get(sUrl, function(res) {
+            var body = "";
+            res.setEncoding("UTF-8");
+            res.on("data", function(chunk) {
+                body += chunk;
+            });
+            res.on("end", function() {
+                callback(body);
+            });
+        })
+    }
 }
--- Chapter-10-Testing-And-Debugging/api-testing/test/api-test.js
+++ Chapter-10-Testing-And-Debugging/api-testing/test/api-test.js
@@ -1,10 +1,32 @@
-var expect = require("chai").expect;
+var expect = require("chai").expect,
+    nock = require("nock");
 
 var api = require("../api.js");
 
-describe("sayHello()", function() {
-    it("should return `Hello, John Cena`", function() {
-        var sResult = api.sayHello("John Cena");
-        expect(sResult).to.equal("Hello, John Cena");
+describe("API Testing Suite", function() {
+    describe("sayHello()", function() {
+        it("should return `Hello, John Cena`", function() {
+            var sResult = api.sayHello("John Cena");
+            expect(sResult).to.equal("Hello, John Cena");
+        });
+    });
+
+    describe("loadWiki()", function() {
+
+        before(function() {
+            nock("https://en.wikipedia.org")
+                .get("/wiki/John_Cena")
+                .reply(200, "Mock - John Cena Wiki Page");
+        });
+
+        it("should load John Cena's Wiki", function(done) {
+            api.loadWiki({
+                first: "John",
+                last: "Cena"
+            }, function(html) {
+                expect(html).to.equal("Mock - John Cena Wiki Page");
+                done();
+            });
+        });
     });
 });
